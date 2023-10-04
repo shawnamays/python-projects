81be9117bf8901ed88df1943d916d1df45cf428d
Shawna Mays <shawnadmays@gmail.com>
2023-10-04 18:38:46 -0400

    notes

--- week3flowcontrol.py
+++ week3flowcontrol.py
@@ -1,7 +1,12 @@
 #flow control week 3
 
-current = 8
-end = 88
-while current < end:
-    print(current)
-    current = current + 2
+# current = 8
+# end = 88
+# while current < end:
+#     print(current)
+#     current = current + 2
+
+for a in range(2,100,2):
+    print(a)
+    
+#a function is a predefined algorithm: someone else has already written a series of statements to solve a certain problem
