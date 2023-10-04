8aa536dd1df358b26fe7b66ad44af0e9a6936f2f
Shawna Mays <shawnadmays@gmail.com>
2023-10-04 19:11:50 -0400

    updated exercise

--- week3flowcontrol.py
+++ week3flowcontrol.py
@@ -6,7 +6,16 @@
 #     print(current)
 #     current = current + 2
 
-for a in range(2,100,2):
-    print(a)
+# for a in range(2,100,2):
+#     print(a)
     
 #a function is a predefined algorithm: someone else has already written a series of statements to solve a certain problem
+
+n = input("put a number here")
+#my input will be 10
+currentoddNumber = 1
+#oddCount is an iterating variable
+for oddCount in range(int(n)):
+    print(currentoddNumber)
+    #this is updating the current odd number
+    currentoddNumber = currentoddNumber + 2
