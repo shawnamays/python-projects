3f7d98aba87c7de38f01ed3a6ea4f98b0c1877e4
Shawna Mays <shawnadmays@gmail.com>
2023-10-04 19:20:32 -0400

    second exercise

---
+++ #second exercise.py
@@ -0,0 +1,8 @@
+#second exercise
+
+startingNumber = 20
+
+for countdown in range(0,20):
+    print(startingNumber)
+    #we need to update the starting number with each iteration
+    startingNumber = startingNumber - 1
