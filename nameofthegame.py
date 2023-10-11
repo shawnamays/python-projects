b8b7343e8ef400f73d5999e2e86a0426c010433d
Shawna Mays <shawnadmays@gmail.com>
2023-10-11 19:11:18 -0400

    name game

---
+++ nameofthegame.py
@@ -0,0 +1,12 @@
+#name of the game
+
+
+def nameMatch(otherNames,myName = "Shawna"):
+   
+        if otherNames == myName:
+            print("that's my name too!")
+
+
+otherNames = input("what's your name?")
+
+nameMatch(otherNames,"Shawna")
