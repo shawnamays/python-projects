d0988330c6b57d820c64d2d31e657a4c9f377131
Shawna Mays <shawnadmays@gmail.com>
2023-10-11 18:57:05 -0400

    looping work push

--- looping.py
+++ looping.py
@@ -1,3 +1,14 @@
-def print_list_elements(my_list):
-    for item in my_list:
-        print(item)
+# def print_list_elements(my_list):
+#     for item in my_list:
+#         print(item)
+
+def favorite_food(food_list,myFav = "Tacos"):
+    for food in food_list:
+        if food ==myFav:
+            print(food)
+
+
+food_list = ["tacos","burgers","salad"]
+
+favorite_food(groceries)
+favorite_food(groceries,"salad")
