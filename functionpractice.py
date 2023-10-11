7cc8e9da73553d7c7f6b02181ae9a529398c7ddd
Shawna Mays <shawnadmays@gmail.com>
2023-10-11 18:41:34 -0400

    push with subtract function

--- functionpractice.py
+++ functionpractice.py
@@ -1,10 +1,15 @@
 #functions practice
 
-def add(number1,number2):
-    result = number1 + number2
+# def add(number1,number2):
+#     result = number1 + number2
+#     return result
+
+
+# print(add(5,10))
+# result = add(5,10)
+# print(result)
+
+def subtract(n1,n2):
+    result = n1 - n2
     return result
-
-
-print(add(5,10))
-result = add(5,10)
-print(result)
+print (subtract(10,5))
